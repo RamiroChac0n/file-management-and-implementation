adf87ca43a451cdd19b62a598fbac069085c63b5
Ramiro Chacón <ramiro20010202@gmail.com>
2023-07-31 23:51:10 -0600

    add or update file

--- lab/01/Practica.c
+++ lab/01/Practica.c
@@ -5,11 +5,29 @@ void create_file(char name[50]){
     char filename[54];
     strcpy(filename, name);
     strcat(filename, ".txt");
-    FILE *file = fopen(filename, "w");
+    FILE *file = fopen(filename, "w+");
     if(file == NULL){
         printf("Error al crear el archivo\n");
     }else{
         printf("Archivo creado con exito\n");
+    }
+    fclose(file);
+}
+
+void update_file(char name[50]){
+    char filename[54];
+    strcpy(filename, name);
+    strcat(filename, ".txt");
+    FILE *file = fopen(filename, "a");
+    if(file == NULL){
+        printf("Error al abrir el archivo\n");
+    }else{
+        printf("Archivo abierto con exito\n");
+        char text[200];
+        printf("Ingrese el texto a agregar: ");
+        scanf(" %[^\n]", text);
+        fprintf(file, "%s\n", text);
+        printf("Texto agregado con exito\n");
     }
     fclose(file);
 }
@@ -20,6 +38,7 @@ int main(){
     {
         printf("\n______________________ Menu ______________________\n");
         printf("1. Crear archivo\n");
+        printf("2. Agregar texto a un archivo\n");
         printf("0. Salir\n");
         printf("_________________________________________________\n");
         printf("Opcion:\n");
@@ -32,6 +51,12 @@ int main(){
             printf("Ingrese el nombre del archivo: ");
             scanf(" %[^\n]", name);
             create_file(name);
+            break;
+        case 2:
+            char name2[50];
+            printf("Ingrese el nombre del archivo: ");
+            scanf(" %[^\n]", name2);
+            update_file(name2);
             break;
         case 0:
             printf("Saliendo...\n");
