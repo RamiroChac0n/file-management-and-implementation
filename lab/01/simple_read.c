fc5c1d3e9e5f2588739937ff19d0ca9d7e8ed5a5
Ramiro Chacón <ramiro20010202@gmail.com>
2023-08-01 13:48:49 -0600

    add documentation

--- lab/01/simple_read.c
+++ lab/01/simple_read.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>                                              // printf, fopen, fclose, fgetc
 
 int main(){
-    FILE *fd;
-    char *filename = "archivo.txt";
+    FILE *fd;                                                   // Puntero a FILE
+    char *filename = "archivo.txt";                             // Nombre del archivo
     char c;
 
-    fd = fopen(filename, "r");
+    fd = fopen(filename, "r");                                  // Abrir archivo en modo lectura
     if (fd == NULL)
     {
-        printf("No se pudo abrir el archivo %s\n", filename);
+        printf("No se pudo abrir el archivo %s\n", filename);   // Si no se pudo abrir el archivo
         return 1;
     }
 
-    c = fgetc(fd);
-    while (c != EOF)
+    c = fgetc(fd);                                              // Leer un caracter
+    while (c != EOF)                                            // Mientras no se llegue al final del archivo
     {
-        putchar(c);
-        c = fgetc(fd);
+        putchar(c);                                             // Imprimir el caracter leido
+        c = fgetc(fd);                                          // Leer un caracter
     }
 
-    fclose(fd);
+    fclose(fd);                                                 // Cerrar el archivo
     return 0;
 }
--- lab/01/simple_write.c
+++ lab/01/simple_write.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
+#include <stdio.h>                                              // printf, fopen, fclose, fputc
 #include <string.h>
 
 int main(){
-    FILE  *fd;
+    FILE  *fd;                                                  // Puntero a FILE
     char *cadena = "HOLA DESDE C";
-    char *filename = "archivo.txt";
+    char *filename = "archivo.txt";                             // Nombre del archivo
 
-    fd = fopen(filename, "w+");
+    fd = fopen(filename, "w+");                                 // Abrir archivo en modo escritura
     if (fd == NULL)
     {
-        printf("No se pudo abrir el archivo %s\n", filename);
+        printf("No se pudo abrir el archivo %s\n", filename);   // Si no se pudo abrir el archivo
         return 1;
     }
-    printf("Archivo %s abierto correctamente\n", filename);
+    printf("Archivo %s abierto correctamente\n", filename);     // Si se pudo abrir el archivo
 
-    int len = strlen(cadena);
+    int len = strlen(cadena);                                   // Tamaño de la cadena
     int i;
-    for (i = 0; i < len; i++)
+    for (i = 0; i < len; i++)                                   // Escribir la cadena
     {
-        fputc(cadena[i], fd);
+        fputc(cadena[i], fd);                                   // Escribir un caracter
     }
-    fputc('\n', fd);
-    printf("Datos escritos correctamente\n");
-    fclose(fd);
+    fputc('\n', fd);                                            // Escribir un salto de linea
+    printf("Datos escritos correctamente\n");                   // Si se escribieron los datos
+    fclose(fd);                                                 // Cerrar el archivo
     return 0;
 }
