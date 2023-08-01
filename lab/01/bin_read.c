51e8d6249d880c35d0b979f3f3a6830bf6b3ae9f
Ramiro Chacón <ramiro20010202@gmail.com>
2023-08-01 13:44:18 -0600

    documentation

--- lab/01/bin_read.c
+++ lab/01/bin_read.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+#include <stdio.h>                                              // printf, fopen, fclose, fread
 
 int main(){
-    FILE *fd;
-    char *filename = "number.dat";
-    fd = fopen(filename, "r");
+    FILE *fd;                                                   // Puntero a FILE
+    char *filename = "number.dat";                              // Nombre del archivo
+    fd = fopen(filename, "r");                                  // Abrir archivo en modo lectura
     if (fd == NULL)
     {
-        printf("No se pudo abrir el archivo %s\n", filename);
-        return 1;
+        printf("No se pudo abrir el archivo %s\n", filename);   // Si no se pudo abrir el archivo
+        return 1;                                               // Terminar el programa
     }
-    printf("Archivo %s abierto correctamente\n", filename);
-    int tam_int = sizeof(int);
+    printf("Archivo %s abierto correctamente\n", filename);     // Si se pudo abrir el archivo
+    int tam_int = sizeof(int);                                  // Tamaño de un entero
     int i;
-    int r = fread(&i, tam_int, 1, fd);
-    while (r != 0)
+    int r = fread(&i, tam_int, 1, fd);                          // Leer un entero
+    while (r != 0)                                              // Mientras no se llegue al final del archivo
     {
-        printf("Numero %d leido\n", i);
-        r = fread(&i, tam_int, 1, fd);
+        printf("Numero %d leido\n", i);                         // Imprimir el entero leido
+        r = fread(&i, tam_int, 1, fd);                          // Leer un entero
     }
-    fclose(fd);
+    fclose(fd);                                                 // Cerrar el archivo
     return 0;
 }
