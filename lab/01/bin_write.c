75fca03a85d272cd7735508e04bdba3129116703
Ramiro Chacón <ramiro20010202@gmail.com>
2023-08-01 13:45:54 -0600

    add documentation

--- lab/01/bin_write.c
+++ lab/01/bin_write.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
+#include <stdio.h>                                              // printf, fopen, fclose, fwrite
 
 int main(){
-    FILE *fd;
-    char *filename = "number.dat";
-    fd = fopen(filename, "w+");
-    if (fd == NULL)
+    FILE *fd;                                                   // Puntero a FILE
+    char *filename = "number.dat";                              // Nombre del archivo
+    fd = fopen(filename, "w+");                                 // Abrir archivo en modo escritura
+    if (fd == NULL)                                             
     {
-        printf("No se pudo abrir el archivo %s\n", filename);
+        printf("No se pudo abrir el archivo %s\n", filename);   // Si no se pudo abrir el archivo
         return 1;
     }
-    printf("Archivo %s abierto correctamente\n", filename);
-    int tam_int = sizeof(int);
+    printf("Archivo %s abierto correctamente\n", filename);     // Si se pudo abrir el archivo
+    int tam_int = sizeof(int);                                  // Tamaño de un entero
     int i;
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++)                                    // Escribir 10 enteros
     {
-        fwrite(&i, tam_int, 1, fd);
+        fwrite(&i, tam_int, 1, fd);                             // Escribir un entero
     }
-    printf("Datos escritos correctamente\n");
-    fclose(fd);
+    printf("Datos escritos correctamente\n");                   // Si se escribieron los datos
+    fclose(fd);                                                 // Cerrar el archivo
     return 0;
 }
