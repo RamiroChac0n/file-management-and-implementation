cb866abac005165f8c53477e2a7d4b460f56bee9
Ramiro Chacón <ramiro20010202@gmail.com>
2023-09-12 16:08:00 -0600

    initial commit

---
+++ lab/05/funscads.c
@@ -0,0 +1,18 @@
+cadespace(cad)
+    char cad[];
+{
+    int i;
+
+    for (i = strlen(cad) - 1; i >= 0 && cad[i] == ' '; i--)
+        ;
+
+    cad[++i] = '\0';
+    return(i);
+}
+
+mayusculas(cadent, cadsal)
+    char cadent[], cadsal[];
+{
+    while (*cadsal++ = (*cadent >= 'a' && *cadent <= 'z') ? *cadent & 0x5F : *cadent)
+        cadent++;
+}
