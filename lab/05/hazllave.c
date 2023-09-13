be4db49eefc7bc7d34f8e54e7cddd7df77994d2b
Ramiro Chacón <ramiro20010202@gmail.com>
2023-09-13 10:32:55 -0600

    initial commit

---
+++ lab/05/hazllave.c
@@ -0,0 +1,14 @@
+hazllave(apellido, nombre, cad)
+    char apellido[], nombre[], cad[];
+{
+    int longap, longnom;
+
+    longap = cadespac(apellido);
+    strcpy(cad, apellido);
+    cad[longap++] = ' ';
+    cad[longap] = '\0';
+    longnom = cadespac(nombre);
+    strcat(cad, nombre);
+    mayusculas(cad);
+    return(longap + longnom);
+}
