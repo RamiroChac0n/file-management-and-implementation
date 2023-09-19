bf89221b44aaf6a4d4f7827fa7f9e5c88a4197cf
Ramiro Chacón <ramiro20010202@gmail.com>
2023-09-19 10:45:34 -0600

    fix toma_archent()

--- lab/05/clasifram.c
+++ lab/05/clasifram.c
@@ -1,5 +1,7 @@
 #include "arches.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define LONG_REG 64
 typedef char NODOLLAVE[30];
 typedef char REGDATOS [LONG_REG +1];
@@ -8,9 +10,19 @@ static struct{
     char relleno[30];
 } encabezado;
 
-main(){
+static int toma_archent();
+static int toma_archsal();
+
+static int cadespac();
+static int hazllave();
+static void clasif_shell();
+static int toma_campo();
+static void mayusculas();
+static void extrae_llave();
+
+int main(){
     int fd_ent, fd_sal, i, cont_reg;
-    char *calloc();
+    //char *calloc();
     REGDATOS *registros;
     NODOLLAVE *nodosllave;
     short *indice;
@@ -49,29 +61,31 @@ main(){
 
     close(fd_ent);
     close(fd_sal);
+    return(0);
 }
 
-static toma_archent(){
+static int toma_archent(){
     char nomarch[30];
     int fd;
 
     printf("Proporcione el nombre del archivo de entrada: ");
-    gets(nomarch);
+    fgets(nomarch, 30, stdin);
     if((fd = open(nomarch, LECTESCRIT)) < 0){
         printf("El archivo %s no puede abrirse\n", nomarch);
+        exit(1);
     }else{
         read(fd, &encabezado, sizeof(encabezado));
     }
     return(fd);
 }
 
-static toma_archsal(){
+static int toma_archsal(){
     char nomarch[30];
     int fd;
 
     printf("Proporcione el nombre del archivo de salida: ");
-    gets(nomarch);
-    if((fd = creat(nomarch, 0644/*MODOP*/)) < 0){
+    fgets(nomarch, 30, stdin);
+    if((fd = creat(nomarch, PMODE/*0644MODOP*/)) < 0){
         printf("El archivo %s no puede crearse\n", nomarch);
     }else{
         write(fd, &encabezado, sizeof(encabezado));
@@ -79,23 +93,10 @@ static toma_archsal(){
     return(fd);
 }
 
-static extrae_llave(llave, registro)
-    REGDATOS registro;
-    char llave[];
-{
-    int pos_bus;
-    char nombre[30], apellido[30];
-
-    pos_bus = 0;
-    pos_bus = toma_campo(apellido, registro, pos_bus, LONG_REG);
-    pos_bus = toma_campo(nombre, registro, pos_bus, LONG_REG);
-    hazllave(apellido, nombre, llave);
-}
-
 #define CAMPO_LLAVE(i) nodosllave [indice[(i)]]
 typedef char NODOLLAVE[30];
 
-clasif_shell(indice, nodosllave, n)
+void clasif_shell(indice, nodosllave, n)
     short indice[];
     NODOLLAVE nodosllave[];
     int n;
@@ -116,4 +117,71 @@ clasif_shell(indice, nodosllave, n)
             indice[i + espacio] = subindice_ins;
         }
     }
+}
+
+int toma_campo(campo, buffreg, pos_bus, long_reg)
+    char campo[], buffreg[];
+    short pos_bus, long_reg;
+{
+    short cpos = 0;     /*posicion en el arreglo "campo" */
+
+    if (pos_bus == long_reg) /* si no hay mas campos que leer */
+        return(0);              /*devuelve pos_bus de 0 */
+
+    while( pos_bus < long_reg && (campo[cpos++] = buffreg[pos_bus++]) != DELIM_CAR);
+
+    if (campo[cpos -1] == DELIM_CAR)
+        campo[-cpos] = '\0';
+    else
+        campo[cpos] = '\0';
+
+    return (pos_bus);
+
+}
+
+int cadespac(cad)
+    char cad[];
+{
+    int i;
+
+    for (i = strlen(cad) - 1; i >= 0 && cad[i] == ' '; i--)
+        ;
+
+    cad[++i] = '\0';
+    return(i);
+}
+
+void mayusculas(cadent, cadsal)
+    char cadent[], cadsal[];
+{
+    while (*cadsal++ = (*cadent >= 'a' && *cadent <= 'z') ? *cadent & 0x5F : *cadent)
+        cadent++;
+}
+
+int hazllave(apellido, nombre, cad)
+    char apellido[], nombre[], cad[];
+{
+    int longap, longnom;
+
+    longap = cadespac(apellido);
+    strcpy(cad, apellido);
+    cad[longap++] = ' ';
+    cad[longap] = '\0';
+    longnom = cadespac(nombre);
+    strcat(cad, nombre);
+    mayusculas(cad, cad);
+    return(longap + longnom);
+}
+
+static void extrae_llave(llave, registro)
+    REGDATOS registro;
+    char llave[];
+{
+    int pos_bus;
+    char nombre[30], apellido[30];
+
+    pos_bus = 0;
+    pos_bus = toma_campo(apellido, registro, pos_bus, LONG_REG);
+    pos_bus = toma_campo(nombre, registro, pos_bus, LONG_REG);
+    hazllave(apellido, nombre, llave);
 }
