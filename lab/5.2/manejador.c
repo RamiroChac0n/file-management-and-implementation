7903146c2fdce2018573bab4aa134b1053359999
Ramiro Chacón <ramiro20010202@gmail.com>
2023-09-29 09:55:47 -0600

    add manejador.c

---
+++ lab/5.2/manejador.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "ab.h"
+
+abreab();
+tomaraiz();
+crea_arbol();
+inserta();
+crea_raiz();
+
+int main(){
+    int promovido;      /* booleano: indica si hay una promocion */
+    short   raiz,       /* NRR de la raíz */
+            nrr_promo;  /* NRR que se promueve */
+    char llave_promo,   /* llave que se promueve */
+         llave;         /* Siguiente llave por insertar en el arbol */
+    
+    if(abreab())            /* Intenta abrir arbolb.dat y obtener la raíz */
+        raiz = tomaraiz();
+    else                    /* Si arbolb.dat no existe lo crea */
+        raiz = crea_arbol();
+
+    while((llave = getchar()) != 'q'){
+        promovido = inserta(raiz, llave, &nrr_promo, &llave_promo);
+        if(promovido)      /* Si hubo promoción */
+            raiz = crea_raiz(llave_promo, raiz, nrr_promo);
+    }
+    cierraab();             /* Cierra arbolb.dat */
+}
