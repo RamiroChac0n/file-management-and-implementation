1f2e95fcada894f2a9585b088527b2d7e440aac8
Ramiro Chacón <ramiro20010202@gmail.com>
2023-09-29 12:13:26 -0600

    add abes.c

--- lab/5.2/manejador.c
+++ lab/5.2/manejador.c
@@ -31,3 +31,70 @@ int main(){
     cierraab();             /* Cierra arbolb.dat */
     return 0;
 }
+
+// CÓDIGO DE abes.c
+int daab;
+
+int abreab(){
+    daab = open ("arbolb.dat", READWRITE);
+    return (daab > 0);
+}
+
+void cierraab(){
+    close (daab);
+}
+
+short tomaraiz (){
+    short raiz;
+    long lseek ();
+    lseek (daab, 0L, 0);
+    if (read (daab,&raiz, 2)==0){
+        printf ("Error: No se pudo obtener la raiz. \007\n");
+        exit (1);
+    }
+    return (raiz);
+}
+
+void colocaraiz (raiz)
+short raiz;
+{
+    long lseek ();
+    lseek (daab, 0L, 0);
+    write (daab, &raiz, 2);
+}
+
+short crea_arbol(){
+    char llave;
+    daab = creat ("arbolb.dat", PMODE);
+    close(daab);
+    abreab();   /* Para asegurar el acceso de lectura y escritura. */
+    llave = getchar();  /* Toma la primera llave */
+    return (crea_raiz(llave, NULO, NULO));
+}
+
+short tomapag()
+{
+    long lseek(), dir;
+    dir = lseek(daab, 0L, 2) -2L;
+    return ((short) dir/ TAMPAGINA);
+}
+
+int leeab(nrr, apunt_pagina)
+short nrr;
+PAGINAAB *apunt_pagina;
+{
+    long lseek(), dir;
+    dir = (long)nrr * (long)TAMPAGINA + 2L;
+    lseek(daab, dir, 0);
+    return (read(daab, apunt_pagina, TAMPAGINA));
+}
+
+int escribreab (nrr, apunt_pagina)
+short nrr;
+PAGINAAB *apunt_pagina;
+{
+    long lseek(), dir;
+    dir = (long) nrr * (long) TAMPAGINA + 2L;
+    lseek(daab, dir, 0);
+    return (write(daab, apunt_pagina, TAMPAGINA));
+}
