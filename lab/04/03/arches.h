b45c54ab68cd5e7b4ddd6d149d197bbc35e3b208
Ramiro Chacón <ramiro20010202@gmail.com>
2023-11-14 11:40:18 -0600

    Add new files and functions for lab 04

---
+++ lab/04/03/arches.h
@@ -0,0 +1,13 @@
+#ifndef ARCHES_H
+#define ARCHES_H
+
+#define PMODE 0755
+#define SOLOLECT 0
+#define SOLOESCRIT 1
+#define LECTESCRIT 2
+#define DELIM_CAD "|"
+#define DELIM_CAR '|'
+
+#define TAM_MAX_REG 512
+
+#endif // ARCHES_H
